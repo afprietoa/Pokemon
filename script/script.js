17ccb7fa3872bc58ff9b60dbfcb85cbb40b72b27
Andres Prieto <afprietoa@unal.edu.co>
2021-10-23 13:03:23 -0500

    exs-Pokemon

---
+++ script/script.js
@@ -0,0 +1,42 @@
+//----------------------GET API DATA-------------------//
+
+const getPokemon = async(id) =>{
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+
+    try{
+        const response = await fetch(url)
+        const data = await response.json()
+        //console.log(data);
+        printData(data)
+    }
+    catch(error){
+        throw error
+    }
+
+}
+const getPokemons = (qty) => {
+    let i=1
+    while(i <= qty){
+       getPokemon(i)
+       i++
+    }
+    
+}
+
+let html ='';
+const  printData = (data) => {
+    
+    console.log(data)
+   
+        html += '<div class ="col mt-5" >';
+            html += '<div class="card" style="width: 13rem;">'
+                html += `<img src="${data.sprites.front_default}" class="card-img-top" alt="...">`
+                html += '<div class="card-body">'
+                    html += `<h5 class="card-title text-center" >${data.species.name}</h5>`
+                html += '</div>'
+            html += '</div>'
+        html += '</div>'
+    
+    document.getElementById('cards-Char').innerHTML = html
+}
+getPokemons(9)
